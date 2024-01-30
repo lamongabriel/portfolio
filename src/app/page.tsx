fd8c8fa6078a7e17bf99bf3415de3767d12e3c2b
Gabriel Lamon <lamongabriel@icloud.com>
2024-01-30 10:08:46 -0300

    updated home screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,8 @@
-import Image from "next/image";
-
-import louisePic from "@/assets/images/louise.png"
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
+    <main className="flex min-h-screen flex-col items-center justify-center gap-2">
+      <h1>lamongabriel.</h1>
       <h2 className="font-bold">Website under construction</h2>
-      <p>Have a look at this young good looking lady while you wait</p>
-      <br/>
-      <Image 
-        alt="Louise"
-        src={louisePic}
-        height={200}
-        width={200}
-      />
     </main>
   );
 }
