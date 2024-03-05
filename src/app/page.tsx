8028c6b52102ce290caff48f9ee2ea915667c931
Gabriel Lamon <lamongabriel@icloud.com>
2024-03-04 21:09:18 -0300

    added fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-center gap-2">
       <h1>lamongabriel.</h1>
       <h2 className="font-bold">Website under construction</h2>
+      <p>Testing Cabinet Grotesk Font</p>
+      <h1>Testing Satoshi Font</h1>
     </main>
   );
 }
