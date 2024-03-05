5d82797681f714d7be851698d2390945807344b2
Gabriel Lamon <lamongabriel@icloud.com>
2024-03-04 21:52:24 -0300

    added colors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import Logo from '@/components/ui/Logo'
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center gap-2">
-      <Logo />
+      <div>
+        <h1 className="text-lg-green-2 text-[61px] font-black leading-tight">
+          hey, <br /> i'm {'{ gabriel lamon }'}
+        </h1>
+      </div>
     </main>
   )
 }
