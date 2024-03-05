b74171511e536c0a4df9bddcb84f409464bc9739
Gabriel Lamon <lamongabriel@icloud.com>
2024-03-04 21:26:36 -0300

    added logo to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
+import Logo from '@/components/ui/Logo'
+
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center gap-2">
-      <h1>lamongabriel.</h1>
-      <h2 className="font-bold">Website under construction</h2>
-      <p>Testing Cabinet Grotesk Font</p>
-      <h1>Testing Satoshi Font</h1>
+      <Logo />
     </main>
   )
 }
